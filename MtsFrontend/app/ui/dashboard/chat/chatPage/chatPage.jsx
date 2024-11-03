0bf0661127cb8793f138d070f355b2a3f8373629
snchs <cazhdanov@mail.ru>
2024-11-04 01:14:27 +0300

    correct json

--- MtsFrontend/app/ui/dashboard/chat/chatPage/chatPage.jsx
+++ MtsFrontend/app/ui/dashboard/chat/chatPage/chatPage.jsx
@@ -19,7 +19,7 @@ const ChatPage = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(message),
+        body: JSON.stringify({text: message}),
       });
 
       if (response.ok) {
